740d9811d760b17c271de11cf7181cbd4eda9f8c
NDVvV <viet.nguyenduc.shust@gmail.com>
2023-03-03 16:29:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function App() {
+
+  const [counter, setCounter] = useState(1)
+
+  const handleIncrease = () => {
+     setCounter(counter + 1)
+  }
+
+  return (
+    <div className="App" style={{padding: 20}}>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  );
+}
+
+export default App;
